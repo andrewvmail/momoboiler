d5904728120c58d986d2ae443b34f771955657e0
Andrew Tan <andrewvmail@gmail.com>
2018-03-05 21:45:17 -0800

    init project and commit working build

---
+++ README.md
@@ -0,0 +1,8 @@
+# momo boiler
+
+```
+git clone  ..
+npm install
+find . -name "*.msj" -type f -delete #https://github.com/webpack/webpack/issues/6459
+webpack --watch --mode development
+```  
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+// var ExtractTextPlugin = require(path.join(cordovaNodeModules, 'extract-text-webpack-plugin'));
+
+
+module.exports = {
+  // webpack folder's entry js - excluded from jekll's build process.
+  entry: "./src/entry.js",
+  output: {
+    path: path.resolve(__dirname, 'www/js/'),
+    filename: "index.js"
+  },
+  resolve: {
+    alias: {
+      'react': 'inferno-compat',
+      'react-dom': 'inferno-compat'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: [["inferno", {
+            "imports": true
+          }]]
+        }
+      },
+      // {
+      //   test: /\.css$/,
+      //   include: [
+      //     path.join(__dirname, 'node_modules', 'onsenui', 'css-components-src', 'src'),
+      //     path.join(__dirname, 'src')
+      //   ],
+      //   loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1&-raw!postcss')
+      // }, {
+      //   test: /\.css$/,
+      //   exclude: [
+      //     path.join(__dirname, 'node_modules', 'onsenui', 'css-components-src', 'src'),
+      //     path.join(__dirname, 'src')
+      //   ],
+      //   loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+      // }
+      // {
+      //   test: /\.svg$/,
+      //   loader: 'file',
+      //   query: {
+      //     name: 'static/media/[name].[hash:8].[ext]'
+      //   }
+      // },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?\S*)?$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      }
+    ]
+  },
+
+
+
+};
